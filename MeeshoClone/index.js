7142e32ae5469c3434bee188439ad7415b8b07cb
Shakti Modak <shaktimodak81@gmail.com>
2023-01-09 17:02:03 +0530

    madeApp

---
+++ MeeshoClone/index.js
@@ -0,0 +1,17 @@
+import { registerRootComponent } from "expo";
+
+import App from "./src/App";
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { name as appName } from "./app.json";
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+registerRootComponent(Main);
